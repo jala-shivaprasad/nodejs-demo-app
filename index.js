8aa7be47a8e08d78cbb053c8df59d6bbdc0f4152
JALA SHIVA PRASAD <jshiva38787@gmail.com>
2026-07-23 12:56:57 +0530

    Initialize Express server with a greeting endpoint

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js CI/CD Demo App!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

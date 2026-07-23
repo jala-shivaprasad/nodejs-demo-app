c1afbdbfa8cad7454df41979db393b5cb1616a2d
JALA SHIVA PRASAD <jshiva38787@gmail.com>
2026-07-23 07:48:47 +0000

    Implement CI/CD pipeline

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Node.js CI/CD Demo App!");
+    res.send("Hello from Node.js CI/CD Demo App! This is Elevate Labs Internship 23/07/2026");
 });
 
 const PORT = process.env.PORT || 3000;
